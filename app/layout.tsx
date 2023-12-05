6679319d60f501fa822c7098b1c174e31be49021
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-05 19:38:11 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'ShopLite',
-  description: 'An eccormmerce marketplace for small business owners to sell their goods and potential customers to find quality products',
+  description: 'An ecormmerce marketplace for small business owners to sell their goods and potential customers to find quality products',
 }
 
 export default function RootLayout({
@@ -16,8 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='h-full'>
-      <body className={`${inter.className} font-sans relative h-full antialiased`}>
-        {children}
+      <body className={cn('font-sans relative h-full antialiased`', inter.className )}>
+       <main className='flex flex-col min-h-screen relative'>
+           <div className='flex-grow flex-1'> {children}</div>
+       </main>
       </body>
     </html>
   )
