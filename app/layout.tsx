abd2f70ad650f6a0e9f5c02c690427ce21d1ff64
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-05 19:19:38 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ShopeLite',
+  title: 'ShopLite',
   description: 'An eccormmerce marketplace for small business owners to sell their goods and potential customers to find quality products',
 }
 
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='h-full'>
+      <body className={`${inter.className} font-sans relative h-full antialiased`}>
+        {children}
+      </body>
     </html>
   )
 }
