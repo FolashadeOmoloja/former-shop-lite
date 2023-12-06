682f535a34d875934fb9e6fd581d1bca991770d1
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-06 14:49:18 +0100

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import MaxWidthWrapper from '@/components/Elements/MaxWidthWrapper'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-           Hello world
-           
-    </main>
+        <MaxWidthWrapper>
+          
+        </MaxWidthWrapper>
   )
 }
