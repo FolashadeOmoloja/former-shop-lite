d56573a5fc72c0456e4a1e13808e954d6d63b0b1
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-07 23:15:49 +0100

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import MaxWidthWrapper from '@/components/Elements/MaxWidthWrapper'
+import Header from '@/components/Home/Header/Header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
       <section>
-        
+        <Header/>
       </section>
   )
 }
