c11ce96bf1e1b1dcd0c1135f244d3fd0d68a8055
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-08 12:08:06 +0100

    header

--- components/Home/Header/Header.tsx
+++ components/Home/Header/Header.tsx
@@ -4,15 +4,15 @@ import Navbar from './Navbar/Navbar'
 
 const Header = () => {
   return (
-       <MaxWidthWrapper className='relative h-screen'>
-         <div  className='flex h-full  absolute top-0 left-0 right-0'>
+   <section className='relative h-screen'>
+      <div  className='flex h-full  absolute top-0 left-0 right-0'>
          <div  className='basis-1/2 bg-white '></div>
          <div className='basis-1/2 bg-[#417394]'></div>
-         </div>
-         <div className='relative z-4'>
+    </div>
+        <MaxWidthWrapper className='relative h-screen z-4'>
              <Navbar/>
-        </div>
        </MaxWidthWrapper>
+   </section>
   )
 }
 
